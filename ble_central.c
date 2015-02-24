a78e7ed34d656087809e75ce1a8213ed5a863c65
Alexander Lea <alexander.lea@gmail.com>
2015-02-24 13:57:56 +0000

    reading stdout line by line

--- ble_central.c
+++ ble_central.c
@@ -38,10 +38,20 @@ int main(int argc, char *argv[])
         //See if whitelist broadcast devices are in range
         //Connect if they are, raise error if not
         
-        //Assuming C4:4F:B7:B1:41:D7 is in range
-        system("sudo gatttool -b C4:4F:B7:B1:41:D7 --char-write-req --handle=0x000f --value=0100 --listen");
+        //Assuming C4:4F:B7:B1:41:D7 is in range and connectable
+        //system("sudo gatttool -b C4:4F:B7:B1:41:D7 --char-write-req --handle=0x000f --value=0100 --listen");
+        FILE *ble_listen = popen("sudo gatttool -b C4:4F:B7:B1:41:D7 --char-write-req --handle=0x000f --value=0100 --listen", "r");        
+        char buf[1024];              
         
-        //read each line of incoming text, and do something with it
+        //read each line of incoming text
+        while (fgets(buf, sizeof(buf), ble_listen) != 0) {
+            printf("%s", buf);
+        }
+        
+        //log it in transaction log
+        //send it on
+        
+        pclose(ble_listen);
     }
     else
     {   //PARENT PROCESS
