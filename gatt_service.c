307f553872e7cfadb2f0e78e38a88810d5099770
Alexander Lea <alexander.lea@gmail.com>
2015-02-24 15:46:05 +0000

    Added gatt_service.c, in which a custom gatt service will be created and run

---
+++ gatt_service.c
@@ -0,0 +1,10 @@
+#include <config.h>
+#endif
+#include <glib.h>
+#include <bluetooth/uuid.h>
+#include "plugin.h"
+#include "hcid.h"
+#include "log.h"
+#include "attrib-server.h"
+#include "att.h"
+
