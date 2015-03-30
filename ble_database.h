4c80bae133065fe9290223dbfb17f9aefb68101c
Alexander Lea <alexander.lea@gmail.com>
2015-03-30 14:54:00 +0100

    changes to database header file

--- ble_database.h
+++ ble_database.h
@@ -1,6 +1,11 @@
 #ifndef BLE_DATABASE_H_
 #define BLE_DATABASE_H_
 
+#include <sqlite3.h>
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+
 int server_log_add(char *source, char *message);
 
 int comms_log_add(char *direction, char *from, char *contents, int log_type);
