f7a9eb6cce8ccac7c189fa8daaca57de1772b1d8
Alexander Lea <alexander.lea@gmail.com>
2015-03-31 13:36:34 +0100

    getting value from incoming BLE connection

--- ble_central.c
+++ ble_central.c
@@ -1,7 +1,5 @@
 /*
 * Compile using: gcc ble_central.c -o ble_central
-* db = carDB; tbl = log; sql = create table log(id integer primary key, sender text, recipient text, service_uuid integer, command integer);
-*
 *
 *   BLE node MAC 	= C4:4F:B7:B1:41:D7
 *	PC MAC 			= 00:02:72:D9:FA:ED
@@ -18,72 +16,94 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#include "ble_database.h"
+//#include "ble_database.h"
 
 extern char **environ;
 
-char * getValue(char *_input)
+/*char * getValue(char *_input)
 {
-	char **ap, *argv[10], *inputstring;
+	/*char **ap, *valueString;
 
-   	for (ap = argv; (*ap = strsep(&inputstring, " \t")) != NULL;)
+   	for (ap = valueString; (*ap = strsep(&_input, " \t")) != NULL;)
            if (**ap != '\0')
-                   if (++ap >= &argv[10])
-                           break;
+                   if (++ap >= &valueString[10])
+                           break; 
                                    
-	return 	argv[sizeof(argv)];
-}
+	return 	valueString;
+	
+	
+	char sub[1000];
+   	int position = 36, length = 23, c = 0;
+	 
+	while (c < length) {
+      		sub[c] = _input[position+c-1];
+      		c++;
+   	}
+   	sub[c] = '\0';
+ 	
+ 	return sub;
+}*/
 
 int main(int argc, char *argv[])
 {
-    //setup GATT server
-//how to call serverUp from gatt_Service.c - need to include in makefile	serverUp();
+	//setup GATT server
 
-    //proces ID
-    pid_t pid;
-    pid_t wpid;
 
-    //fork the process
-    pid = fork();
-    if (pid == -1) {
-        printf("Failed to fork()\n"); 
-        //TODO: return valid error here, rather than exiting
-        exit(13);
-    }
+	//Scan for devices (should this be done in another thread?)
+	//Foreach device in whitelist, do:
+	
+	//process IDs
+   	pid_t pid;
+    	pid_t wpid;
+
+    	//fork the process
+    	pid = fork();
+	if (pid == -1) {
+		printf("Failed to fork()\n"); 
+        	//TODO: return valid error here, rather than exiting
+		exit(13);
+    	}
     
-    //Fork was successful
-    if (pid == 0) { //CHILD PROCESS
-
-        //Scan for devices (should this be done in another thread?)
-        //See if whitelist broadcast devices are in range
-        //Connect if they are, raise error if not
+    	//Fork was successful
+    	if (pid == 0) { //CHILD PROCESS        
         
-        //Assuming C4:4F:B7:B1:41:D7 is in range and connectable
-        //system("sudo gatttool -b C4:4F:B7:B1:41:D7 --char-write-req --handle=0x000f --value=0100 --listen");
-        FILE *ble_listen = popen("sudo gatttool -b C4:4F:B7:B1:41:D7 --char-write-req --handle=0x000f --value=0100 --listen", "r");        
-        char buf[1024];              
+		//Assuming C4:4F:B7:B1:41:D7 is in range and connectable
+		FILE *ble_listen 
+			= popen("sudo gatttool -b C4:4F:B7:B1:41:D7 --char-write-req --handle=0x000f --value=0100 --listen", "r");        
+		char buf[1024];              
         
-        //read each line of incoming text
-        while (fgets(buf, sizeof(buf), ble_listen) != 0) {
-            //printf("%s", buf);
-            //printf("Value = %s", getValue(buf));
-            
-            //log it in transaction log
-	        //send it on
-	        
-//	        sendCommand();
-        }
-               
-        pclose(ble_listen);
-    }
-    else
-    {   //PARENT PROCESS
-        int status;
-        
-        //Wait for the child to exit
-        wpid = wait(&status);
-        
-    }
+		//read each line of incoming text
+		while (fgets(buf, sizeof(buf), ble_listen) != 0) {
+			//printf("%s", buf);
+		   	//printf("Value = %s", getValue(buf));
+		    
+			//want substring of: Notification handle = 0x000e value: 2a 20 00 20 10 3f 00 20
+			//TODO: There must be a nicer way of doing this!
+			char *sub;
+			int position = 37, length = 23, c = 0;
+		 
+			while (c < length) {
+	      			sub[c] = buf[position+c-1];
+	      			c++;
+	   		}
+	   		sub[c] = '\0';
+		    
+		    	printf("%s\n", sub);
+		    	
+			//log it in transaction log
+			//send it on			
+		}
+		       
+		pclose(ble_listen);
+	}
+	else
+    	{   //PARENT PROCESS
+		int status;
+		
+		//Wait for the child to exit
+		wpid = wait(&status);
+		
+    	}
     
-    return 0;
+    	return 0;
 }
