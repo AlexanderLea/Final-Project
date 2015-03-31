1faa61f2d77afd0dba7f80559c4812f88958f0ea
Alexander Lea <alexander.lea@gmail.com>
2015-03-31 17:20:51 +0100

    updated header to match changes in .c

--- ble_database.h
+++ ble_database.h
@@ -10,4 +10,8 @@ int server_log_add(char *source, char *message);
 
 int comms_log_add(char *direction, char *from, char *contents, int log_type);
 
+void getListWhitelistMAC(int _whitelistSize, char *data[_whitelistSize]);
+
+int getWhitelistSize();
+
 #endif
