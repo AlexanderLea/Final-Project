77315b4a56e55758586cba7501713b6d48d6ee1b
Alexander Lea <alexander.lea@gmail.com>
2015-04-02 13:23:31 +0100

    Tidied stuff up - comments and indentation and prettyness

--- nodejs_central/gatt_central.js
+++ nodejs_central/gatt_central.js
@@ -1,91 +1,64 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
-
 var noble = require('noble');
 
+/** UUID declaration */
 var carServiceUuid		= '2a67';
 var carCharacteristicUuid 	= '1817';
 var errorServiceUuid = '';
 
+/** Variable to hold carCharacteristic */
+var carCharacteristic = null;
+
 noble.on('stateChange', function(state) {
-	if (state === 'poweredOn') {
-		//
-		// Once the BLE radio has been powered on, it is possible
-		// to begin scanning for services. Pass an empty array to
-		// scan for all services (uses more time and power).
-		//
+	if (state === 'poweredOn') { //If bluetooth, scan
 		console.log('scanning...');
 		noble.startScanning();
 	}
-	else {
+	else { //don't scan
 		noble.stopScanning();
 	}
 });
 
-var carCharacteristic = null;
-
+/** Listen to onDiscover to hopefully discover some devices. */
 noble.on('discover', function(peripheral) {
-	//
-	// The advertisment data contains a name, power level (if available),
-	// certain advertised service uuids, as well as manufacturer data,
-	// which could be formatted as an iBeacon.
-	//
-
+	
+	//We found one!
 	console.log('found peripheral:', peripheral.advertisement.localName);
-	//
-	// Once the peripheral has been discovered, then connect to it.
-	// It can also be constructed if the uuid is already known.
-	///
+	
+	//Connect to everything!! TODO: only connect to whitelist devices
 	peripheral.connect(function(err) {
 		console.log('conntected to: ', peripheral.advertisement.localName);
 		
-		//
-		// Once the peripheral has been connected, then discover the
-		// services and characteristics of interest.
-		//
+		//We are now connected, so discover if it exposes carServiceUuid
 		peripheral.discoverServices([carServiceUuid], function(err, services) {
 			services.forEach(function(service) {
-				//
-				// This must be the service we were looking for.
-				//
+				
+				//If it's found, it must have that service running
 				console.log('found car comms service:', service.uuid);
 
-				//
 				// So, discover its characteristics.
-				//
 				service.discoverCharacteristics([], function(err, characteristics) {
-
 					characteristics.forEach(function(characteristic) {
-						//
-						// Loop through each characteristic and match them to the
-						// UUIDs that we know about.
-						//
-						console.log('found car comms characteristic:', characteristic.uuid);
+						
+						//found a characteristic
+						console.log('found characteristic:', characteristic.uuid, ' in service: ', service.uuid);
 
+						//is it the characteristic we're looking for?
+						//TODO: Perhaps car and error should be characteristics in the same service rather than different services?
 						if (carCharacteristicUuid == characteristic.uuid) {
+							//yes it is!
+							//get characteristic object
 							carCharacteristic = characteristic;
 						
-						//TODO: Perhaps car and error should be characteristics in the same service???
-						
-							//listen
+							//read characteristic value
 							carCharacteristic.on('read', function(data, isNotification) {
 	     						//Buffer buf = data;
 	     						console.log('data', data);
 							});
 
-							// true to enable notify
+							//enable notifications so we get updates
 							carCharacteristic.notify(true, function(error) {
 								console.log('listening...');
 							});
-	
 						}
 						else {
 							console.log('cannot find car comms characteristic');
@@ -96,18 +69,3 @@ noble.on('discover', function(peripheral) {
 		});
 	});
 });
-  
-function blah() {
-	console.log('Oooh data');
-	/*carCharacteristic.on('read', function(data, isNotification) {
-		console.log('Oooh data');
-            
-		if (data.length === 1) { //check data validity
-	 		var result = data.readUInt8(0);
-	  		//get data
-		}
-		else {
-	 		console.log('result length incorrect')
-		}
-	});*/
-}
