bbfb2cfe7af845bf743adbdd8aa0baef8fc3c214
Alexander Lea <alexander.lea@gmail.com>
2015-04-04 13:02:33 +0100

    using new models. untested

--- nodejs_central/gatt_central.js
+++ nodejs_central/gatt_central.js
@@ -4,77 +4,17 @@
 */
 
 var noble = require('noble');
-var observer = require('./gatt_central_observer.js');
+var observer = require('./gatt_central_observer');
+var broadcaster = require('/gatt_central_peripheral')
 
+var gattObserver = new observer();
+var gattBroadcaster = new broadcaster();
 
+gattBroadcaster.run();
 
-//Here downwards is now in gatt_central_observer.js
-/** UUID declaration */
-var carServiceUuid		= '2a67';
-var carCharacteristicUuid 	= '1817';
-var errorServiceUuid = '';
-
-/** Variable to hold carCharacteristic */
-var carCharacteristic = null;
-
-noble.on('stateChange', function(state) {
-	if (state === 'poweredOn') { //If bluetooth, scan
-		console.log('scanning...');
-		noble.startScanning();
-	}
-	else { //don't scan
-		noble.stopScanning();
-	}
+gattObserver.run(function(err, data){
+	//return when command is recieved
+	
+	gattObserver.sendCommand('command');
 });
 
-/** Listen to onDiscover to hopefully discover some devices. */
-noble.on('discover', function(peripheral) {
-	
-	//We found one!
-	console.log('found peripheral:', peripheral.advertisement.localName);
-	
-	//Connect to everything!! TODO: only connect to whitelist devices
-	peripheral.connect(function(err) {
-		console.log('conntected to: ', peripheral.advertisement.localName);
-		
-		//We are now connected, so discover if it exposes carServiceUuid
-		peripheral.discoverServices([carServiceUuid], function(err, services) {
-			services.forEach(function(service) {
-				
-				//If it's found, it must have that service running
-				console.log('found car comms service:', service.uuid);
-
-				// So, discover its characteristics.
-				service.discoverCharacteristics([], function(err, characteristics) {
-					characteristics.forEach(function(characteristic) {
-						
-						//found a characteristic
-						console.log('found characteristic:', characteristic.uuid, ' in service: ', service.uuid);
-
-						//is it the characteristic we're looking for?
-						//TODO: Perhaps car and error should be characteristics in the same service rather than different services?
-						if (carCharacteristicUuid == characteristic.uuid) {
-							//yes it is!
-							//get characteristic object
-							carCharacteristic = characteristic;
-						
-							//read characteristic value
-							carCharacteristic.on('read', function(data, isNotification) {
-	     						//Buffer buf = data;
-	     						console.log('data', data);
-							});
-
-							//enable notifications so we get updates
-							carCharacteristic.notify(true, function(error) {
-								console.log('listening...');
-							});
-						}
-						else {
-							console.log('cannot find car comms characteristic');
-						}
-					});
-				});
-			});
-		});
-	});
-});
