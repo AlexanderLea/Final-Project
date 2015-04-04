133749234c2747f5b94b285acf10ebb78fe7a90b
Alexander Lea <alexander.lea@gmail.com>
2015-04-04 12:49:23 +0100

    turned into a class

--- nodejs_central/gatt_central_peripheral.js
+++ nodejs_central/gatt_central_peripheral.js
@@ -9,39 +9,48 @@ var CarService = require('./car_service');
 
 var name = 'BLE_Central';
 
-//initialise carService
-var carService = new CarService();
+var carService;
 
-//
-// Wait until the BLE radio powers on before attempting to advertise.
-// If you don't have a BLE radio, then it will never power on!
-//
-bleno.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    //
-    // We will also advertise the service ID in the advertising packet,
-    // so it's easier to find.
-    //
-    bleno.startAdvertising(name, [carService.uuid], function(err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-  else {
-    bleno.stopAdvertising();
-  }
-});
+//TODO: on connection and disconnection events
 
-bleno.on('advertisingStart', function(err) {
-  if (!err) {
-    console.log('advertising...');
-    //
-    // Once we are advertising, it's time to set up our services,
-    // along with our characteristics.
-    //
-    bleno.setServices([
-      carService
-    ]);
-  }
-});
+function gattPeripheral(){
+	carService =  = new CarService();
+}
+
+function run(){
+	//
+	// Wait until the BLE radio powers on before attempting to advertise.
+	// If you don't have a BLE radio, then it will never power on!
+	//
+	bleno.on('stateChange', function(state) {
+	  if (state === 'poweredOn') {
+		//
+		// We will also advertise the service ID in the advertising packet,
+		// so it's easier to find.
+		//
+		bleno.startAdvertising(name, [carService.uuid], function(err) {
+		  if (err) {
+		    console.log(err);
+		  }
+		});
+	  }
+	  else {
+		bleno.stopAdvertising();
+	  }
+	});
+
+	bleno.on('advertisingStart', function(err) {
+	  if (!err) {
+		console.log('advertising...');
+		//
+		// Once we are advertising, it's time to set up our services,
+		// along with our characteristics.
+		//
+		bleno.setServices([
+		  carService
+		]);
+	  }
+	});
+}
+
+module.exports = gattPeripheral;
