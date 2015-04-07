c387c2910b483cab6e3940b8dbeaa8054c0c612c
Alexander Lea <alexander.lea@gmail.com>
2015-04-07 17:17:16 +0100

    attempt to make work simulatenously. it doesn't

--- nodejs_central/gatt_central.js
+++ nodejs_central/gatt_central.js
@@ -3,26 +3,58 @@
 * Based fairly heavily on the demos on the wiki
 */
 
-var noble = require('noble');
+var noble = require('noble'),
+	async = require('async');
+	
 var GattObserver = require('./gatt_central_observer');
-var GattPeripheral = require('./gatt_central_peripheral')
+var GattPeripheral = require('./gatt_central_peripheral');
 
-//var gattPeripheral = new GattPeripheral();
+//Instantiate objects
+var gattPeripheral = new GattPeripheral();
 var gattObserver = new GattObserver();
 
-//gattPeripheral.run();
+var peripheralRunning = false, observerRunning = false;
 
-gattObserver.run();
+//if Bluetooth is on, let's go
+noble.on('stateChange', function(state) {
+	if (state === 'poweredOn') {
+		/*
+		async.parallel([
+			function(callback){
+				//console.log('peripheral');
+				gattPeripheral.run();
+				callback(null, 'advertising');
+			},
+			function(callback){
+				//console.log('observer');			
+				gattObserver.run();
+				callback(null, 'observing');
+			}
+		],
+		function(err, data){
+		 	console.log(data);
+		});*/
+		
+		gattObserver.run(function(err){
+			if(!err){
+				observerRunning = true;
+				gattPeripheral.run(function(err){
+					if(!err){
+						peripheralRunning = true;
+					}
+				});
+			} else {
+				console.log('err: ', err);
+			}
+		});		
+	}
+	else { //don't scan		
+		gattPeripheral.stop();
+		gattObserver.stop();
+	}
+});
 
 gattObserver.on('data-recieved', function(data){
-	console.log('server ', data);
+	console.log('recieved ', data);
+	gattPeripheral.broadcastCommand("poo");
 });
-
-/*function(err, data){		
-	//return when command is recieved
-	
-	g
-	console.log('Central: ', data);
-	//gattObserver.sendCommand('command');
-});
-*/
