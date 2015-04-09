25702e084627ed2f180df3ea6d3868821c55fb96
Alexander Lea <alexander.lea@gmail.com>
2015-04-09 12:37:40 +0100

    added ability to queue comms logs too

--- nodejs_central/server_log_queue.js
+++ nodejs_central/server_log_queue.js
@@ -1,17 +1,30 @@
 var	async = require('async'),
 	db = require('./log_database')
 
-var dbQueue = async.queue(function(data, callback){
+var serverDbQueue = async.queue(function(record, callback){
 	//for each item in queue, call save to db method, replacing any null
 	// character values in the string
-	db.serverLogAdd(data.source, data.message.replace('\0', ''));
-	console.log(data);
+	db.serverLogAdd(record.source, record.message.replace('\0', ''), record.priority);
+	console.log(record);
 	callback();
 }, 2);
 
 //Callback for when the queue is empty
-dbQueue.drain = function(){
+serverDbQueue.drain = function(){
 	//console.log('all items have been processed');
 }
 
-module.exports = dbQueue;
+//direction, from, message, logType
+var commsDbQueue = async.queue(function(record, callback){
+
+	db.commsLogAdd(record.direction, record.from, record.message.replace('\0', ''), record.priority);
+	console.log(record);
+	callback();
+}, 2);
+
+//Callback for when the queue is empty
+commsDbQueue.drain = function(){
+	//console.log('all items have been processed');
+}
+
+module.exports = {serverDbQueue: serverDbQueue, commsDbQueue: commsDbQueue};
