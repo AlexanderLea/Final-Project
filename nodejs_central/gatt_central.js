b07fdf4eb210f84acfbc53f8b49255ad522c7eb3
Alexander Lea <alexander.lea@gmail.com>
2015-04-10 16:52:59 +0100

    calling both parts successfully using relative device IDs

--- nodejs_central/gatt_central.js
+++ nodejs_central/gatt_central.js
@@ -2,6 +2,8 @@
 * Using https://github.com/sandeepmistry/noble, which using Bluez
 * Based fairly heavily on the demos on the wiki
 */
+process.env.NOBLE_HCI_DEVICE_ID=0;
+process.env.BLENO_HCI_DEVICE_ID=1;
 
 var noble = require('noble'),
 	async = require('async');
@@ -18,19 +20,37 @@ var peripheralRunning = false, observerRunning = false;
 //if Bluetooth is on, let's go
 noble.on('stateChange', function(state) {
 	if (state === 'poweredOn') {		
-		
-		gattObserver.run(function(err){
-			if(!err){
-				observerRunning = true;
-				//gattPeripheral.run(function(err){
-				//	if(!err){
-				//		peripheralRunning = true;
-				//	}
-				//});
-			} else {
-				console.log('err: ', err);
+		async.parallel([
+			//start Gatt Observer
+			function(observerCallback){
+				gattObserver.run(function(err){
+					if(err){
+						observerCallback(err);
+					} else {
+						observerRunning = true;
+						observerCallback();
+					}
+				});
+			},
+			//start Gatt Peripheral
+			function(peripheralCallback){
+				gattPeripheral.run(function(err){
+					if(err){
+						peripheralCallback(err);
+					} else {
+						observerRunning = true;
+						peripheralCallback();
+					}
+				});
 			}
-		});		
+		], function(err){
+			if(err){
+				console.log(err);
+			} else {
+				console.log('both running!');
+			}
+		});
+		
 	}
 	else { //don't scan		
 		gattPeripheral.stop();
