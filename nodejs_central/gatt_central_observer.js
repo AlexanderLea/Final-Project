57b607f1d57f5d877c47d5945f328440b6f7b665
Alexander Lea <alexander.lea@gmail.com>
2015-04-13 14:51:36 +0100

    rewritten to be smaller and more efficient. TODO: deal with multiple devlies

--- nodejs_central/gatt_central_observer.js
+++ nodejs_central/gatt_central_observer.js
@@ -8,8 +8,10 @@ console.log('observer using device: ', process.env.NOBLE_HCI_DEVICE_ID);
 var noble = require('noble'),
 	events = require('events'),
 	sys = require('sys'),
+	async = require('async'),
 	slog = require('./server_log_queue').serverDbQueue,
-	clog = require('./server_log_queue').commsDbQueue;
+	clog = require('./server_log_queue').commsDbQueue,
+	db = require('./api/whitelist_db');
 
 /** UUID declaration */
 var carServiceUuid		= '2a67';
@@ -30,67 +32,86 @@ sys.inherits(GattObserver, events.EventEmitter);
 
 GattObserver.prototype.run = function(callback){
 	var self = this;
-
-	noble.startScanning();
-	//console.log('scanning');
-	//log in database
-	slog.push({source: dbSource, message: 'scanning for peripherals', priority: 'info'});
 		
-	/** Listen to onDiscover to hopefully discover some devices. */
-	noble.on('discover', function(peripheral) {
-	
-		if(peripheral.advertisement.localName != 'BLE_Central'){
-			//We found one!
-			//log in database
-			slog.push({source: dbSource, message: peripheral.advertisement.localName + ': found', priority: 'info'});
-		
-			//Connect to everything!! TODO: only connect to whitelist devices		
-			peripheral.connect(function(err) {
-				//log in database
-				slog.push({source: dbSource, message: peripheral.advertisement.localName + ': connected', priority: 'info'});
+	async.waterfall([
+		function(whitelistCallback){
 			
-				//We are now connected, so discover if it exposes carServiceUuid
-				peripheral.discoverServices([carServiceUuid], function(err, services) {
-					services.forEach(function(service) {
-				
-						//If it's found, it must have that service running
-						//log in database
-						slog.push({source: dbSource, message: peripheral.advertisement.localName + ': found service: ' + service.uuid, priority: 'info'});
-
-						// So, discover if it has the carChatacteristic
-						//TODO: Perhaps car and error should be characteristics in the same service rather than different services?
-						service.discoverCharacteristics([carCharacteristicUuid], function(err, characteristics) {
-							characteristics.forEach(function(characteristic) {
-													
-								//get characteristic object
-								carCharacteristic = characteristic;
-					
-								//read characteristic value
-								carCharacteristic.on('read', function(data, isNotification) { 					
-			 						//console.log('observer data', data);
-			 						self.emit('data-recieved', data);
-									//direction, from, message, logType
-			 						clog.push({
-			 							direction: 'IN', 
-			 							from: 'TODO: '+ peripheral.advertisement.localName,
-			 							message: data.toString('hex'),
-			 							logType: '1'
-		 							});
-								});
-
-								//enable notifications so we get updates
-								carCharacteristic.notify(true, function(error) {
-									//log in database
-									slog.push({source: dbSource, message: peripheral.advertisement.localName + ': listening for notifications', priority: 'info'});									
-								});
-							
-								callback(null);
-							});
-						});
+			var whitelist = new Array();
+			db.whitelistAll(function(err, rows){
+				if(!err){
+					rows.forEach(function(row){
+						whitelist.push(row.mac_addr.toLowerCase());		
 					});
-				});
+				
+					whitelistCallback(null, whitelist);
+				}
+				else {
+					whitelistCallback(err);
+				}
+			});
+		},
+		function(whitelist, callback){
+	
+			noble.startScanning();
+			slog.push({
+				source: dbSource, 
+				message: 'scanning for peripherals', 
+				priority: 'info'
+			});		
+		
+			//This won't deal with multiple devices!!
+			noble.on('discover', function(peripheral) {
+
+				//if MAC is in whitelist
+				if(whitelist.indexOf(peripheral.address) > -1){
+					//connect
+					peripheral.connect(function(err) {
+						//log in database
+						slog.push({
+							source: dbSource, 
+							message: peripheral.address + ': connected', 
+							priority: 'info'
+						});
+						
+						peripheral.discoverSomeServicesAndCharacteristics(
+							[carServiceUuid], [carCharacteristicUuid], 
+							function(err, services, characteristics){
+						
+							carCharacteristic = characteristics[0];
+
+							//read characteristic value
+							carCharacteristic.on('read', function(data, isNotification) { 					
+		 						//console.log('observer data', data);
+		 						self.emit('data-recieved', data);
+								//direction, from, message, logType
+		 						clog.push({
+		 							direction: 'IN', 
+		 							from: 'peripheral.address,
+		 							message: data.toString('hex'),
+		 							logType: '1'
+	 							});
+							});
+
+							//enable notifications so we get updates
+							carCharacteristic.notify(true, function(error) {
+								//log in database
+								slog.push({
+									message: peripheral.address 
+										+ ': listening for notifications', 
+									priority: 'info'
+								});									
+							});						
+						});
+					});						
+				}
+				else {
+					callback(null, 'nay');
+				}
 			});
 		}
+	], function(err, result){
+		//handle errors
+		console.log(result);
 	});
 }
 
