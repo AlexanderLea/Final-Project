b7054ef588d3eebff0cf2dbd6842841934f0ce5b
Alexander Lea <alexander.lea@gmail.com>
2015-04-14 11:46:49 +0100

    undid changes

--- node/lib/car_service.js
+++ node/lib/car_service.js
@@ -1,18 +1,15 @@
 var util = require('util'),
 	bleno = require('bleno'),
 	CarCharacteristic = require('./car_characteristic');
-	ErrCharacteristic = require('./error_characteristic');
 var BlenoPrimaryService = bleno.PrimaryService;
 
 var carCharacteristic = new CarCharacteristic();
-var errCharacteristic = new ErrCharacterisitc();
 
 function CarService() {
     CarService.super_.call(this, {
         uuid: '2a67',
         characteristics: [
-            carCharacteristic,
-            errCharacteristic
+            carCharacteristic
         ]
     });   
 }
@@ -24,8 +21,5 @@ CarService.prototype.sendCommand = function(cmd){
 	carCharacteristic.updateCharacteristicValue(cmd);
 }
 
-CarService.prototype.reportError = function(_err){
-	errCharacteristic.updateCharacteristicValue(_err);
-}
 
 module.exports = CarService;
