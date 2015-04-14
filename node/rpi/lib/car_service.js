74da166751e115a0b1424d07adf521f650e1bbd8
Alexander Lea <alexander.lea@gmail.com>
2015-04-14 11:52:22 +0100

    removed car characteristic from rpi

--- node/rpi/lib/car_service.js
+++ node/rpi/lib/car_service.js
@@ -1,28 +1,20 @@
 var util = require('util'),
 	bleno = require('bleno'),
-	CarCharacteristic = require('./car_characteristic');
 	ErrCharacteristic = require('./error_characteristic');
 var BlenoPrimaryService = bleno.PrimaryService;
 
-var carCharacteristic = new CarCharacteristic();
 var errCharacteristic = new ErrCharacterisitc();
 
 function CarService() {
     CarService.super_.call(this, {
         uuid: '2a67',
         characteristics: [
-            carCharacteristic,
             errCharacteristic
         ]
     });   
 }
 
 util.inherits(CarService, BlenoPrimaryService);
-
-/**  */
-CarService.prototype.sendCommand = function(cmd){
-	carCharacteristic.updateCharacteristicValue(cmd);
-}
 
 CarService.prototype.reportError = function(_err){
 	errCharacteristic.updateCharacteristicValue(_err);
